c4d13761365424b923df8f973b1d577056d7c10e
Tshepiso Mokano <tshepisomokano@gmail.com>
2026-02-19 10:46:25 +0200

    ...

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.sgbenefits.co.za',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
